77bb5b8fbf37caa10fb534cd399c30a1292103ce
Linus2228 <Linus2228@gmail.com>
2019-10-24 17:57:47 +0300

    snapshot test

--- src/Ring/Ring.test.js
+++ src/Ring/Ring.test.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import { Ellipsis } from '..'
+import React from "react";
+import renderer from "react-test-renderer";
+import { Ellipsis } from "..";
 
-it('renders correctly', () => {
-  const tree = renderer.create(<Ellipsis />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+it("renders correctly", () => {
+  const tree = renderer.create(<Ellipsis />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
